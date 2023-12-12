527e16a6d0f16f23e5fae8dfd8621545ecbe665e
ivan <shanin-i2011@yandex.ru>
2023-12-12 20:59:42 +0300

    feat(products): add Category model

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from autoslug import AutoSlugField
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(
+        populate_from="name", unique=True, db_index=True, always_update=True
+    )
+    is_active = models.BooleanField(default=True)
+    parent_category = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    @staticmethod
+    def get_default_category():
+        return Category.objects.get_or_create(
+            name="Без категории", is_active=False
+        )[0]
