e56cca1a217ad279cf304f9b1af3be0d04ffaf20
ivan <shanin-i2011@yandex.ru>
2023-12-14 13:29:51 +0300

    feat(admin): register models for admin panel

--- src/apps/products/admin.py
+++ src/apps/products/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.products.models import Category, Image, Product, Size
+
+admin.site.register([Category, Image, Size])
+
+
+class SizesInline(admin.TabularInline):
+    model = Product.sizes.through
+
+
+class ImagesInline(admin.TabularInline):
+    model = Product.images.through
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        SizesInline,
+        ImagesInline,
+    ]
+    exclude = ["sizes", "images"]
