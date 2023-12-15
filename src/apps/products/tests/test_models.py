3004a2bcd15dca706f0ad7e7bf579f4f40db9854
ivan <shanin-i2011@yandex.ru>
2023-12-15 21:00:06 +0300

    test(products): add test for 'preview' functionality of ProductImage model
    Add a test in the `test_models.py` file to verify the functionality of the 'preview' feature in the `ProductImage` model. The test ensures that only one image per product can be set as the 'preview'. The `mock_image_save` parameter is used as a mock object for image saving.

--- src/apps/products/tests/test_models.py
+++ src/apps/products/tests/test_models.py
@@ -1,6 +1,7 @@
 import pytest
 
 from apps.products.models import Category
+from apps.products.tests.factories import ProductFactory, ProductImageFactory
 
 
 @pytest.mark.django_db
@@ -16,3 +17,23 @@ def test_get_default_category():
 
     category2 = Category.get_default_category()
     assert category1.id == category2.id
+
+
+@pytest.mark.django_db
+def test_product_image_save(mock_image_save):
+    """
+    Test the 'preview' functionality of the `ProductImage` model.
+    It asserts that only one image per product can be set as 'preview'.
+    The `mock_image_save` parameter is a mock object for image saving.
+    """
+    product = ProductFactory()
+    image1 = ProductImageFactory(product=product, is_preview=True)
+    image1.save()
+    image1.refresh_from_db()
+    assert image1.is_preview
+
+    image2 = ProductImageFactory(product=product, is_preview=True)
+    image1.refresh_from_db()
+
+    assert not image1.is_preview
+    assert image2.is_preview
