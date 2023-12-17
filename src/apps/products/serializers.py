751aa2f33f71d30f16461a8c66ef6e7497be2b87
ivan <shanin-i2011@yandex.ru>
2023-12-17 18:37:43 +0300

    feat(products): add child_categories field to CategorySerializer

--- src/apps/products/serializers.py
+++ src/apps/products/serializers.py
@@ -4,6 +4,10 @@ from apps.products.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    child_categories = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
     class Meta:
         model = Category
         fields = "__all__"
