c706f171b0e7d996fbcd9d9f0bb97719c529e780
ivan <shanin-i2011@yandex.ru>
2023-12-17 18:36:54 +0300

    feat: integrate django_filters into REST_FRAMEWORK settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_spectacular',
+    'django_filters',
 
     'apps.products',
 ]
@@ -155,4 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
 }
