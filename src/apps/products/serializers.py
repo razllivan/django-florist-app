1cb8c44d77cbe3ef0249492846d9fc6fc8e43e0b
ivan <shanin-i2011@yandex.ru>
2023-12-20 22:43:49 +0300

    refactor(products): update serializers to use ModelSerializer and PrimaryKeyRelatedField

--- src/apps/products/serializers.py
+++ src/apps/products/serializers.py
@@ -1,32 +1,30 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from apps.products.models import Category, Image, Product, Size
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    child_categories = serializers.PrimaryKeyRelatedField(
-        many=True, read_only=True
-    )
+class CategorySerializer(ModelSerializer):
+    child_categories = PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Category
         fields = "__all__"
 
 
-class SizeSerializer(serializers.ModelSerializer):
+class SizeSerializer(ModelSerializer):
     class Meta:
         model = Size
         fields = "__all__"
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(ModelSerializer):
     class Meta:
         model = Image
         fields = "__all__"
 
 
-class ProductWriteSerializer(serializers.ModelSerializer):
-    categories = serializers.PrimaryKeyRelatedField(
+class ProductWriteSerializer(ModelSerializer):
+    categories = PrimaryKeyRelatedField(
         many=True, required=False, queryset=Category.objects.all()
     )
     sizes = SizeSerializer(many=True)
