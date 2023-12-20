2a6ddc469999d1b5560f90c8eeccd288c02e82f5
ivan <shanin-i2011@yandex.ru>
2023-12-20 13:51:34 +0300

    feat(products): register ProductViewSet in products urls

--- src/apps/products/urls.py
+++ src/apps/products/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.products.views import CategoryViewSet, ImageViewSet, SizeViewSet
+from apps.products.views import (
+    CategoryViewSet,
+    ImageViewSet,
+    ProductViewSet,
+    SizeViewSet,
+)
 
 app_name = "products"
 
@@ -9,6 +14,7 @@ router = DefaultRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"sizes", SizeViewSet)
 router.register(r"images", ImageViewSet)
+router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
