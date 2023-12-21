6ae4bc57a1959bf3e20d792523bd886f6f4edb6c
ivan <shanin-i2011@yandex.ru>
2023-12-21 17:41:56 +0300

    test(products): add test for 'no_categories' filter

---
+++ src/apps/products/tests/test_filters.py
@@ -0,0 +1,35 @@
+import pytest
+
+from apps.products.filters import ProductFilter
+from apps.products.models import Product
+from apps.products.tests.factories import CategoryFactory, ProductFactory
+
+
+@pytest.mark.django_db
+def test_no_categories_filter_excludes_categorized_products():
+    """
+    Test 'no_categories' filter functionality for products.
+
+    This test ensures that when the 'no_categories' filter is applied to the
+    Product queryset, it returns only the products without any associated
+    categories, and excludes those with associated categories.
+    """
+    category = CategoryFactory()
+
+    # create products with categories and without
+    product_with_category = ProductFactory(
+        name="With category", categories=[category]
+    )
+    product_with_category.categories.add(category)
+    product_without_category = ProductFactory(name="Without category")
+
+    # apply filter 'no_categories'
+    no_categories_filter = ProductFilter(
+        {"no_categories": True}, queryset=Product.objects.all()
+    )
+
+    # Check that a product without a category is returned by the filter
+    assert product_without_category in no_categories_filter.qs
+
+    # Check that a product with a category is not returned by the filter
+    assert product_with_category not in no_categories_filter.qs
