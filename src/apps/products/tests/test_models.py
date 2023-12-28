6070cbc94534cd47b3da844b1211305112784f26
ivan <shanin-i2011@yandex.ru>
2023-12-28 21:14:10 +0300

    test(models): refactor test_only_one_image_per_product_as_preview to use product fixture

--- src/apps/products/tests/test_models.py
+++ src/apps/products/tests/test_models.py
@@ -1,19 +1,16 @@
 import pytest
 
-from apps.products.tests.factories import ProductFactory, ProductImageFactory
+from apps.products.tests.factories import ProductImageFactory
 
 
 @pytest.mark.django_db
-def test_only_one_image_per_product_as_preview(mock_image_save):
+def test_only_one_image_per_product_as_preview(mock_image_save, product):
     """
     Test the 'preview' functionality of the `ProductImage` model.
     It asserts that only one image per product can be set as 'preview'.
     The `mock_image_save` parameter is a mock object for image saving.
     """
-    product = ProductFactory()
     image1 = ProductImageFactory(product=product, is_preview=True)
-    image1.save()
-    image1.refresh_from_db()
     assert image1.is_preview
 
     image2 = ProductImageFactory(product=product, is_preview=True)
