df1d4f9e7764715e2b218db8b3688b52f7204943
ivan <shanin-i2011@yandex.ru>
2023-12-28 18:22:46 +0300

    test(products): Add fixtures for API client, product, image, and product image
    Added fixtures for the API client, product, image, and product image in the
    conftest.py file. These fixtures will be used in tests to set up the necessary
    objects for testing. The fixtures include:
    - api_client: Returns an instance of APIClient for making API requests.
    - product: Returns a ProductFactory instance for creating a product object.
    - image_no_save: Returns an ImageFactory instance without saving image file
    - product_image_no_save: Returns a ProductImageFactory instance without saving image file

--- src/apps/conftest.py
+++ src/apps/conftest.py
@@ -1,4 +1,11 @@
 import pytest
+from rest_framework.test import APIClient
+
+from apps.products.tests.factories import (
+    ImageFactory,
+    ProductFactory,
+    ProductImageFactory,
+)
 
 
 @pytest.fixture()
@@ -15,3 +22,23 @@ def mock_image_save(monkeypatch):
     monkeypatch.setattr(
         "django.core.files.storage.FileSystemStorage.save", mock_save
     )
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def product(db):
+    return ProductFactory()
+
+
+@pytest.fixture
+def image_no_save(db, mock_image_save):
+    return ImageFactory
+
+
+@pytest.fixture
+def product_image_no_save(db, mock_image_save):
+    return ProductImageFactory()
