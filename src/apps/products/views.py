d91bc5ebaa10d1f227d042ed8a0079cc229e57f9
ivan <shanin-i2011@yandex.ru>
2023-12-28 18:16:27 +0300

    feat(product-images): add ProductImagesViewSet to handle CRUD operations for product images

--- src/apps/products/views.py
+++ src/apps/products/views.py
@@ -1,15 +1,17 @@
-from drf_spectacular.utils import extend_schema, extend_schema_view
-from rest_framework.parsers import MultiPartParser
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.products.filters import ProductFilter
-from apps.products.models import Category, Image, Product, Size
+from apps.products.models import Category, Image, Product, ProductImage, Size
 from apps.products.serializers import (
     CategorySerializer,
     ImageSerializer,
-    ProductReadSerializer,
-    ProductWriteSerializer,
+    ProductImageSerializer,
     ProductSerializer,
+    SizeSerializer,
 )
 
 
@@ -54,7 +56,58 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
 
-    def get_serializer_class(self):
-        if self.request.method in ["GET"]:
-            return ProductReadSerializer
-        return ProductWriteSerializer
+class ProductImagesViewSet(ModelViewSet):
+    serializer_class = ProductImageSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def get_queryset(self):
+        if getattr(self, "swagger_fake_view", False):
+            # Return a fake queryset for schema generation
+            return ProductImage.objects.none()
+        product_id = self.kwargs["product_id"]
+        return ProductImage.objects.filter(product_id=product_id)
+
+    def retrieve(self, request, *args, **kwargs):
+        image_id = kwargs.get("pk")
+        product_id = self.kwargs.get("product_id")
+        image = get_object_or_404(
+            ProductImage, image_id=image_id, product_id=product_id
+        )
+        serializer = self.get_serializer(image)
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        image_id = kwargs.get("pk")
+        product_id = self.kwargs.get("product_id")
+        image = get_object_or_404(
+            ProductImage, image_id=image_id, product_id=product_id
+        )
+        serializer = self.get_serializer(
+            image, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        product_id = self.kwargs.get("product_id")
+        product = get_object_or_404(Product, pk=product_id)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(product=product)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        product_id = self.kwargs.get("product_id")
+        get_object_or_404(Product, pk=product_id)
+        return super().list(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        image_id = kwargs.get("pk")
+        product_id = self.kwargs.get("product_id")
+        image = get_object_or_404(
+            ProductImage, image_id=image_id, product_id=product_id
+        )
+        image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
