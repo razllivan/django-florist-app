c5753d11edc5f47e664bd26544ac1ad4e84287a6
ivan <shanin-i2011@yandex.ru>
2023-12-30 11:55:20 +0300

    feat(serializers): add size_description field to ProductImageSerializer

--- src/apps/products/serializers.py
+++ src/apps/products/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import CharField
 from rest_framework.serializers import (
     ImageField,
     ModelSerializer,
@@ -30,20 +31,40 @@ class ImageSerializer(ModelSerializer):
 class ProductImageSerializer(ModelSerializer):
     image = ImageSerializer(read_only=True)
     new_image = ImageField(write_only=True)
+    size_description = CharField(write_only=True, required=False)
 
     def create(self, validated_data):
-        new_image_data = validated_data.pop("new_image")
-        image_instance = Image.objects.create(img=new_image_data)
+        fields_to_process = ["size_description"]
+
+        image_data = {
+            field: validated_data.pop(field)
+            for field in fields_to_process
+            if field in validated_data
+        }
+
+        image_data["img"] = validated_data.pop("new_image")
+
+        image_instance = Image.objects.create(**image_data)
         validated_data["image"] = image_instance
+
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        # sourcery skip: use-named-expression
-        new_image_data = validated_data.pop("new_image", None)
+        fields_to_process = ["new_image", "size_description"]
 
-        if new_image_data:
-            image_instance = Image.objects.create(img=new_image_data)
+        image_data = {
+            field: validated_data.pop(field)
+            for field in fields_to_process
+            if field in validated_data
+        }
+
+        if image_data.get("new_image"):
+            image_data["img"] = image_data.pop("new_image")
+            image_instance = Image.objects.create(**image_data)
             instance.image = image_instance
+        else:
+            for field_name, field_data in image_data.items():
+                setattr(instance.image, field_name, field_data)
         instance.save()
         return super().update(instance, validated_data)
 
@@ -51,6 +72,7 @@ class ProductImageSerializer(ModelSerializer):
         model = ProductImage
         fields = [
             "new_image",
+            "size_description",
             "image",
             "is_preview",
         ]
