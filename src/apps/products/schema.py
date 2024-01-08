89ce66cb7c127453f3e354c736d8b0d5a0a2de9b
ivan <shanin-i2011@yandex.ru>
2024-01-08 21:24:39 +0300

    docs(schema): add ProductImagesSchema class

--- src/apps/products/schema.py
+++ src/apps/products/schema.py
@@ -40,3 +40,45 @@ class ProductSizesSchema:
             To delete the size itself you need to use the `/api/sizes`
             endpoint""",
         )
+
+
+class ProductImagesSchema:
+    def list(self):
+        return extend_schema(
+            summary="Get all images related to the product",
+            description="Get all images related to the product",
+        )
+
+    def create(self):
+        return extend_schema(
+            summary="Add image to product and fill associated fields",
+            description="Add image to product and fill associated fields",
+        )
+
+    def retrieve(self):
+        return extend_schema(
+            summary="Get info about a specific image and its association "
+            "info with a product",
+            description="Get info about a specific image and its association "
+            "info with a product",
+        )
+
+    def partial_update(self):
+        return extend_schema(
+            summary="Update image information about association "
+            "with a product",
+            description="""Updates only image information that belongs
+            to a specific product.
+            <br>
+            If you need to change information about
+            the image itself, use the `/api/images` endpoint""",
+        )
+
+    def destroy(self):
+        return extend_schema(
+            summary="Remove product image",
+            description="""Remove only association between image and product.
+            <br>
+            To delete the image itself you need to use the `/api/images`
+            endpoint""",
+        )
