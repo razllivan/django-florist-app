a0e7dd5b18683069de1ee62931d44bd1d2e3ecd9
ivan <shanin-i2011@yandex.ru>
2024-01-11 10:37:48 +0300

    fix(filters): fix issue with duplicate data in ProductFilter

--- src/apps/products/filters.py
+++ src/apps/products/filters.py
@@ -1,11 +1,19 @@
 import django_filters
+from django_filters import BaseInFilter, NumberFilter
 
 from apps.products.models import Product
 
 
+class NumberInFilter(BaseInFilter, NumberFilter):
+    pass
+
+
 class ProductFilter(django_filters.FilterSet):
     no_categories = django_filters.BooleanFilter(
-        field_name="categories", lookup_expr="isnull"
+        field_name="categories", lookup_expr="isnull", distinct=True
+    )
+    categories = NumberInFilter(
+        field_name="categories", lookup_expr="in", distinct=True
     )
 
     class Meta:
