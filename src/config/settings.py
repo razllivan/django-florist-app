8fb04453eef737b9f2105c4afa1e2b1008634352
ivan <shanin-i2011@yandex.ru>
2024-01-17 17:05:47 +0300

    feat: enable caching via cachalot package

--- src/config/settings.py
+++ src/config/settings.py
@@ -26,6 +26,23 @@ SECRET_KEY = 'django-insecure-as-vnpebt#zi)2=0vz=4x29d5esga!3&vtcs9+kq*!i5_+dlf2
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', False)
+if DEBUG:
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'cachalot.panels.CachalotPanel',
+    ]
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +62,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'debug_toolbar',
+    'cachalot',
 
     'apps.products',
 ]
